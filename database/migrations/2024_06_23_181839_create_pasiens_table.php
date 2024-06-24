45b6939cee5495282af7f87e31159b6ce2f5a7bb
adindanazmisiregar <adindanazmisrgr@gmail.com>
2024-06-24 05:49:25 -0700

    initial commit

---
+++ database/migrations/2024_06_23_181839_create_pasiens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_pasien',40);
+            $table->string('alamat_pasien',35);
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('jenis_kelamin',25);
+            $table->string('diagnosis',20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasien');
+    }
+};
---
+++ database/migrations/2024_06_23_181907_create_riwayats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riwayat', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('kamar_id');
+            $table->unsignedBigInteger('pasien_id');
+            $table->datetime('tanggal_masuk');
+            $table->datetime('tanggal_keluar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riwayat');
+    }
+};
